addcd514972da600a7452ad7761c37529a5244f5
Scott Currie <scurrie@tableau.com>
2019-06-24 12:18:44 -0700

    Removed import. Calling super and passing  x and y now

--- roguelike/player.py
+++ roguelike/player.py
@@ -1,3 +1,7 @@
-class Player(object):
-    def __init__(self):
+from .character import Character
+# from roguelike.character import Character
+
+class Player(Character):
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.symbol = '@'
