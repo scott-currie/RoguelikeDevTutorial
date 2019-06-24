9d7df175400eefa885fd2716651c511a828f4526
Scott Currie <scurrie@tableau.com>
2019-06-24 12:17:40 -0700

    Player position now properties of Player class

--- main.py
+++ main.py
@@ -19,18 +19,19 @@ def main():
             if col == '.':
                 x_pos, y_pos = x, y
                 break
-    x_pos_prev, y_pos_prev = -1, -1
-    
+    # player.x_prev, player.y_prev = -1, -1
+    player = Player(x_pos, y_pos)
+
     tcod.console_print(0, 0, 0, ''.join([''.join(s) + '\n' for s in lvl_map.terrain]))
     
-    player = Player()
+
     
     while not tcod.console_is_window_closed():
-        tcod.console_put_char(0, x_pos, y_pos, player.symbol, tcod.BKGND_NONE)
+        tcod.console_put_char(0, player.x, player.y, player.symbol, tcod.BKGND_NONE)
         tcod.console_flush()
         kp = tcod.console_check_for_keypress()
-        next_x_pos = x_pos
-        next_y_pos = y_pos
+        next_x_pos = player.x
+        next_y_pos = player.y
         if kp.vk == tcod.KEY_ESCAPE:
             return True
         elif kp.vk == tcod.KEY_DOWN:
@@ -41,15 +42,17 @@ def main():
                 next_x_pos += 1
         elif kp.vk == tcod.KEY_LEFT:
                 next_x_pos -= 1
+        
         if lvl_map.space_is_legal(next_x_pos, next_y_pos):
             # Current position becomes next position
-            x_pos, y_pos = next_x_pos, next_y_pos
+        #     x_pos, y_pos = next_x_pos, next_y_pos
+            player.move(next_x_pos, next_y_pos)
             # Print position at bottom of window
-            tcod.console_print(0, 0, SCR_HEIGHT - 1, str(x_pos).zfill(2) + ',' + str(y_pos).zfill(2))
+            tcod.console_print(0, 0, SCR_HEIGHT - 1, str(player.x).zfill(2) + ',' + str(player.y).zfill(2))
             # Draw terrain in space just left
-            tcod.console_put_char(0, x_pos_prev, y_pos_prev, lvl_map.terrain[y_pos_prev][x_pos_prev], tcod.BKGND_NONE)
+            tcod.console_put_char(0, player.x_prev, player.y_prev, lvl_map.terrain[player.y_prev][player.x_prev], tcod.BKGND_NONE)
             # Set prev space to current
-            x_pos_prev, y_pos_prev = x_pos, y_pos
+            player.x_prev, player.y_prev = player.x, player.y
 
 
 
