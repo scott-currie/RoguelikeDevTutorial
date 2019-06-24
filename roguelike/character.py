f3ef984cca43c332714875c65e3ead135a86e7c1
Scott Currie <scurrie@tableau.com>
2019-06-24 12:18:02 -0700

    Added x and y, move functions

--- roguelike/character.py
+++ roguelike/character.py
@@ -1,3 +1,10 @@
 class Character(object):
-    def __init__(self):
-        pass
+    def __init__(self, x, y):
+        self.x = x
+        self.x_prev = -1
+        self.y = y
+        self.y_prev = -1
+
+    def move(self, dest_x, dest_y):
+        self.x = dest_x
+        self.y = dest_y
