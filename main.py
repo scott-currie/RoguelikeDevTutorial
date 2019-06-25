d9f537d1bda5799512d1e13de71ea32e35f626eb
Scott Currie <scurrie@tableau.com>
2019-06-25 08:30:43 -0700

    Changed player instantiation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tcod
 from roguelike.map import Map
 from roguelike.space import Space
 from roguelike.player import Player
+from roguelike.engine import Engine
 
 
 def main():
@@ -14,17 +15,11 @@ def main():
     
     lvl_map = Map(SCR_HEIGHT - 1, SCR_WIDTH)
     
-    for y, row in enumerate(lvl_map.terrain):
-        for x, col in enumerate(row):
-            if col == '.':
-                x_pos, y_pos = x, y
-                break
-    # player.x_prev, player.y_prev = -1, -1
-    player = Player(x_pos, y_pos)
+    player = Player(*lvl_map.get_random_legal_space())
 
-    tcod.console_print(0, 0, 0, ''.join([''.join(s) + '\n' for s in lvl_map.terrain]))
-    
-
+    for row in lvl_map.spaces:
+        for space in row:
+            tcod.console_put_char(0, space.x, space.y, space.terrain, tcod.BKGND_NONE)
     
     while not tcod.console_is_window_closed():
         tcod.console_put_char(0, player.x, player.y, player.symbol, tcod.BKGND_NONE)
@@ -45,14 +40,17 @@ def main():
         
         if lvl_map.space_is_legal(next_x_pos, next_y_pos):
             # Current position becomes next position
-        #     x_pos, y_pos = next_x_pos, next_y_pos
             player.move(next_x_pos, next_y_pos)
             # Print position at bottom of window
             tcod.console_print(0, 0, SCR_HEIGHT - 1, str(player.x).zfill(2) + ',' + str(player.y).zfill(2))
-            # Draw terrain in space just left
-            tcod.console_put_char(0, player.x_prev, player.y_prev, lvl_map.terrain[player.y_prev][player.x_prev], tcod.BKGND_NONE)
+            # Draw terrain in space just vacated
+            tcod.console_put_char(0, player.x_prev, player.y_prev, lvl_map.spaces[player.y_prev][player.x_prev].terrain, tcod.BKGND_NONE)
             # Set prev space to current
             player.x_prev, player.y_prev = player.x, player.y
+            show_neigbors(lvl_map, player)
+
+def show_neigbors(lvl_map, player):
+    print(f'n={lvl_map.spaces[player.y][player.x].n},e={lvl_map.spaces[player.y][player.x].e},s={lvl_map.spaces[player.y][player.x].s},w={lvl_map.spaces[player.y][player.x].w}')
 
 
 
