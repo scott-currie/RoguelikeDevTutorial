bfe87eec99e66e1f384f3c18fd295fd5ceb5974f
Scott Currie <scurrie@tableau.com>
2019-06-25 08:31:43 -0700

    Got rid of terrain list and made it a property of spaces

--- roguelike/map.py
+++ roguelike/map.py
@@ -5,26 +5,35 @@ class Map():
     def __init__(self, rows, cols):
         self.height = rows
         self.width = cols
-        self.spaces = self.make_spaces(rows, cols)
-        self.terrain = [['.'] * cols for _ in range(rows)]
+        self.spaces = self.make_spaces()
         self.make_walls()
         self.make_obstacles()
 
 
-    def make_spaces(self, rows, cols):
-        spaces = [[None for _ in range(cols)] for _ in range(rows)]
-        for row in range(rows):
-            for col in range(cols):
-                spaces[row][col] = Space(row, col)
+    def make_spaces(self):
+        ''' Generate and link space objects into the map table.
+        '''
+        # Init spaces to a table of Nones
+        spaces = [[None for _ in range(self.width)] for _ in range(self.height)]
+        for row in range(self.height):
+            for col in range(self.width):
+                new_space = Space(row, col)
+                # import pdb; pdb.set_trace()
+                if row > 0:
+                    new_space.n = spaces[col][row - 1]
+                if col > 0:
+                    new_space.w = spaces[col - 1][row]
+                spaces[row][col] = new_space
         return spaces
 
+
     def make_walls(self):
-        for i in range(len(self.terrain)):
-            for j in range(len(self.terrain[0])):
-                if i == 0 or i == len(self.terrain) - 1:
-                    self.terrain[i][j] = '#'
-                if j == 0 or j == len(self.terrain[i]) - 1:
-                    self.terrain[i][j] = '#'
+        for i in range(len(self.spaces)):
+            for j in range(len(self.spaces[0])):
+                if i == 0 or i == len(self.spaces) - 1:
+                    self.spaces[i][j].terrain = '#'
+                if j == 0 or j == len(self.spaces[i]) - 1:
+                    self.spaces[i][j].terrain = '#'
 
 
     def make_obstacles(self):
@@ -33,7 +42,7 @@ class Map():
         while len(obstacles) < n_obs:
             space = random.randint(1, self.width - 2), random.randint(1, self.height - 2)
             obstacles.add(space)
-            self.terrain[space[1]][space[0]] = 'X'
+            self.spaces[space[1]][space[0]].terrain = 'X'
 
 
     def space_is_in_bounds(self, x, y):
@@ -44,8 +53,15 @@ class Map():
     
     
     def space_is_passable(self, x, y):
-        return self.terrain[y][x] == '.'
+        return self.spaces[y][x].terrain == '.'
 
 
     def space_is_legal(self, x, y):
         return self.space_is_in_bounds(x, y) and self.space_is_passable(x, y)
+
+
+    def get_random_legal_space(self):
+        space = 0, 0
+        while self.spaces[space[0]][space[1]].terrain != '.':
+            space = random.randint(1, self.width - 1), random.randint(1, self.height - 1)
+        return space
