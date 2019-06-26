4962ceaeed443cf5cd531c37151d8fb301c88d70
Scott Currie <scurrie@tableau.com>
2019-06-25 17:36:20 -0700

    Added loc property

--- roguelike/space.py
+++ roguelike/space.py
@@ -2,6 +2,7 @@ class Space(object):
     def __init__(self, row, col, terrain='.'):
         self.x = col
         self.y = row
+        self.loc = (self.x, self.y)
         self.terrain = terrain
         self.n = None
         self.s = None
