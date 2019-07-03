d85ddfd57c26ecc581134674bdcfd9464444ba15
Scott Currie <scurrie@tableau.com>
2019-07-03 08:42:42 -0700

    Added print_terrain. Fixed map population code.

--- roguelike/map.py
+++ roguelike/map.py
@@ -1,5 +1,6 @@
 from roguelike.space import Space
 import random
+import sys
 
 class Map():
     def __init__(self, rows, cols):
@@ -7,25 +8,27 @@ class Map():
         self.width = cols
         self.spaces = self.make_spaces()
         self.make_walls()
-        # self.make_obstacles()
+        self.print_terrain()
+        self.make_obstacles()
 
 
     def make_spaces(self):
         ''' Generate and link space objects into map.spaces
         '''
-        # Init spaces to a table of Nones
-        # spaces = [[None for _ in range(self.width)] for _ in range(self.height)]
+        print('Making spaces.')
         spaces = []
+        # Make a list of rows (height)
         for row in range(self.height):
             # line holds items for this row
             line = []
+            spaces.append(line)
             for col in range(self.width):
-                spaces.append(line)
-                new_space = Space(row, col)
+
+                new_space = Space(col, row)
                 line.append(new_space)
                 if row > 0:
                     # Link to space to the north
-                    north = spaces[col][row - 1]
+                    north = spaces[row - 1][col]
                     new_space.n = north
                     # If there is a space to the north, link it to new_space
                     if north is not None:
@@ -37,29 +40,32 @@ class Map():
                     # If there's a space to the west, link it to new_space
                     if west is not None:
                         west.e = new_space
-        print('Linked all the spaces!')
+        print('Made spaces.')
         return spaces
 
 
     def make_walls(self):
+        print('Making walls.')
         for i in range(len(self.spaces)):
             for j in range(len(self.spaces[0])):
-                if i == 0 or i == len(self.spaces) - 1:
+                if (i == 0 or i == self.height - 1) or (j == 0 or j == self.width - 1):
                     self.spaces[i][j].terrain = '#'
-                if j == 0 or j == len(self.spaces[i]) - 1:
-                    self.spaces[i][j].terrain = '#'
-
+                    print(f'wall@({j},{i})')
+                # if j == 0 or j == self.width - 1:
+                #     self.spaces[i][j].terrain = '#'
+                #     print(f'wall@({j},{i})')
+        print('Made walls.')
 
     def make_obstacles(self):
+        print('Making obstacles.')
         obstacles = set()
         n_obs = int((self.height - 2) * (self.width - 2) * .2) 
         while len(obstacles) < n_obs:
-            print('Still making obstacles!')
             space = random.randint(1, self.width - 2), random.randint(1, self.height - 2)
             obstacles.add(space)
             print(len(obstacles))
             self.spaces[space[1]][space[0]].terrain = 'X'
-
+        print('Made obstaccles.')
 
     def space_is_in_bounds(self, x, y):
         if x > 0 and x < self.width - 1:
@@ -77,8 +83,16 @@ class Map():
 
 
     def get_random_legal_space(self):
-        space = -1, -1
-        while self.space_is_legal(space[0], space[1]):
-            space = random.randint(1, self.width - 1), random.randint(1, self.height - 1)
-            print('Finding legal space!')
-        return space
+        while True:
+            x, y = random.randint(1, self.width - 1), random.randint(1, self.height - 1)
+            # print(f'{x},{y},{self.spaces[y][x].terrain}')
+            if self.space_is_legal(x, y):
+                return (x, y)
+
+
+    def print_terrain(self):
+        # print(''.join([''.join([space.terrain] + '\n') for space in self.spaces]))
+        for row in self.spaces:
+            for space in row:
+                sys.stdout.write(space.terrain + ' ')
+            sys.stdout.write('\n')
