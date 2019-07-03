bfc43c3d953a5186ec0e605d9b7304a1f51ac57b
Scott Currie <scurrie@tableau.com>
2019-07-03 08:41:49 -0700

    Added prints for debugging.

--- main.py
+++ main.py
@@ -12,16 +12,17 @@ def main():
     tcod.console_set_custom_font('arial10x10.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
     tcod.console_init_root(SCR_WIDTH, SCR_HEIGHT, 'My cool console.', order='F')
     tcod.console_set_default_foreground(0, tcod.yellow)
-    
+    print('Ready to make map.')
     lvl_map = Map(SCR_HEIGHT - 1, SCR_WIDTH)
-    
+    print('Ready to make player.')
     player = Player(*lvl_map.get_random_legal_space())
-
+    print('Ready to print map to console.')
     for row in lvl_map.spaces:
         for space in row:
             tcod.console_put_char(0, space.x, space.y, space.terrain, tcod.BKGND_NONE)
-    
+    print('Ready to enter main loop.')
     while not tcod.console_is_window_closed():
+        # print(f'Player at {player.loc}')
         tcod.console_put_char(0, player.x, player.y, player.symbol, tcod.BKGND_NONE)
         tcod.console_flush()
         kp = tcod.console_check_for_keypress()
@@ -37,8 +38,9 @@ def main():
                 next_x_pos += 1
         elif kp.vk == tcod.KEY_LEFT:
                 next_x_pos -= 1
-        
+        # import pdb; pdb.set_trace()
         if lvl_map.space_is_legal(next_x_pos, next_y_pos):
+
             # Current position becomes next position
             player.move(next_x_pos, next_y_pos)
             # Print position at bottom of window
@@ -47,7 +49,7 @@ def main():
             tcod.console_put_char(0, player.x_prev, player.y_prev, lvl_map.spaces[player.y_prev][player.x_prev].terrain, tcod.BKGND_NONE)
             # Set prev space to current
             player.x_prev, player.y_prev = player.x, player.y
-            show_neigbors(lvl_map, player)
+            # show_neigbors(lvl_map, player)
 
 def show_neigbors(lvl_map, player):
     if lvl_map.spaces[player.y][player.x].n:
