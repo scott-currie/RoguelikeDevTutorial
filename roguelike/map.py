041a3d94e9ff4434b3542885155430a0e47c42ae
Scott Currie <scurrie@tableau.com>
2019-07-17 16:13:38 -0700

    Eliminated comments and obsolete code.

--- roguelike/map.py
+++ roguelike/map.py
@@ -12,8 +12,7 @@ class Map(_Map):
         self.width = cols
         self.spaces = self.make_spaces()
         self.make_walls()
-        # self.print_terrain()
-        # self.make_obstacles()
+        self.make_obstacles()
 
     def make_spaces(self):
         ''' Generate and link space objects into map.spaces
@@ -28,7 +27,6 @@ class Map(_Map):
         print('Made spaces.')
         return spaces
 
-
     def make_walls(self):
         print('Making walls.')
         for row in range(self.height):
@@ -36,7 +34,6 @@ class Map(_Map):
                 if (col == 0 or col == self.height - 1) or (row == 0 or row == self.width - 1):
                     self.spaces[row][col].terrain = '#'
                     self.walkable[row][col] = False
-                    # print(f'wall@({row},{col})')
         print('Made walls.')
 
     def make_obstacles(self):
@@ -49,16 +46,14 @@ class Map(_Map):
             obstacles.add(space)
             print(len(obstacles))
             self.spaces[space[1]][space[0]].terrain = 'X'
+            self.walkable[space[1]][space[0]] = False
         print('Made obstaccles.')
 
     def space_is_in_bounds(self, row, col):
-        if col > 0 and col < self.width - 1:
-            if row > 0 and row < self.height - 1:
+        if col >= 0 and col <= self.width - 1:
+            if row >= 0 and row <= self.height - 1:
                 return True
         return False
-
-    # def space_is_passable(self, row, col):
-    #     return self.walkable[row][col]
 
     def space_is_legal(self, row, col):
         return self.space_is_in_bounds(row, col) and self.walkable[row][col]
@@ -69,9 +64,3 @@ class Map(_Map):
                 1, self.height - 1), random.randint(1, self.width - 1)
             if self.space_is_legal(row, col):
                 return (row, col)
-
-    def print_terrain(self):
-        for row in self.spaces:
-            for space in row:
-                sys.stdout.write(space.terrain + ' ')
-            sys.stdout.write('\n')
