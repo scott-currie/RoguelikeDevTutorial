dbb5f690bcc28704cf3daf4ef81a70f7847a0c55
Scott Currie <scurrie@tableau.com>
2019-07-17 16:10:23 -0700

    prev eliminated. next initializes to current.

--- roguelike/character.py
+++ roguelike/character.py
@@ -1,11 +1,9 @@
 class Character(object):
     def __init__(self, row, col):
         self.col = col
-        self.col_prev = -1
-        self.col_next = -1
+        self.col_next = col
         self.row = row
-        self.row_prev = -1
-        self.row_next = -1
+        self.row_next = row
         self.loc = self.row, self.col
 
     def move(self, dest_row, dest_col):
