ef9fc7bddba18967c5971d5a495486d894c08c3f
Scott Currie <scurrie@tableau.com>
2019-07-18 08:50:02 -0700

    Added function get_dest

--- roguelike/player.py
+++ roguelike/player.py
@@ -1,7 +1,18 @@
 from .character import Character
-
+import tcod
 
 class Player(Character):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.symbol = '@'
+
+    def get_dest(self, kp):
+        if kp.vk == tcod.KEY_DOWN:
+            self.row_next += 1
+        elif kp.vk == tcod.KEY_UP:
+            self.row_next -= 1
+        elif kp.vk == tcod.KEY_RIGHT:
+            self.col_next += 1
+        elif kp.vk == tcod.KEY_LEFT:
+            self.col_next -= 1
+        return self.row_next, self.col_next
