88dba442e219f7a603b047db9e8c9e70a9ec268e
Scott Currie <scurrie@tableau.com>
2019-07-18 16:16:21 -0700

    Fixed parameters in get_dest

--- roguelike/player.py
+++ roguelike/player.py
@@ -5,8 +5,10 @@ class Player(Character):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.symbol = '@'
+        self.color = tcod.white
 
-    def get_dest(self, kp):
+    def get_dest(self, kp, lvl_map, actors):
+        print('Getting player destination.')
         if kp.vk == tcod.KEY_DOWN:
             self.row_next += 1
         elif kp.vk == tcod.KEY_UP:
