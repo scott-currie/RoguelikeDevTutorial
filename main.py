682fd618373a09e97de91a256fc4469572180905
Scott Currie <scurrie@tableau.com>
2019-07-18 08:49:16 -0700

    Moved movement code to character.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import tcod
 from roguelike.map import Map
 from roguelike.space import Space
 from roguelike.player import Player
-from roguelike.engine import Engine
+# from roguelike.engine import Engine
 
 SCR_WIDTH = 20
 SCR_HEIGHT = 21
+
 
 def main():
     tcod.console_set_custom_font(
@@ -17,51 +18,37 @@ def main():
     lvl_map = Map(SCR_HEIGHT - 1, SCR_WIDTH)
     print('Ready to make player.')
     player = Player(*lvl_map.get_random_legal_space())
-    print('Ready to print map to console.')
-    # Print map to console
-    for row in lvl_map.spaces:
-        for space in row:
-            tcod.console_put_char(0, space.col, space.row,
-                                  space.terrain, tcod.BKGND_NONE)
+    actors = [player]
+    print('Ready to render map to console.')
+    lvl_map.render()
     print('Ready to enter main loop.')
     # Enter main loop
     while not tcod.console_is_window_closed():
-        tcod.console_set_default_foreground(0, tcod.white)
-        tcod.console_put_char(0, player.col, player.row,
-                              player.symbol, tcod.BKGND_NONE)
+        # Pump events for keypress
         kp = tcod.console_check_for_keypress()
         if kp.vk == tcod.KEY_ESCAPE:
             return True
-        elif kp.vk == tcod.KEY_DOWN:
-            player.row_next += 1
-        elif kp.vk == tcod.KEY_UP:
-            player.row_next -= 1
-        elif kp.vk == tcod.KEY_RIGHT:
-            player.col_next += 1
-        elif kp.vk == tcod.KEY_LEFT:
-            player.col_next -= 1
-        # Do player move if next position dfferent than current
-        if player.col_next != player.col or player.row_next != player.row:
-            if lvl_map.space_is_legal(player.row_next, player.col_next):
-                # Draw terrain in current space
-                tcod.console_set_default_foreground(0, tcod.yellow)
-                tcod.console_put_char(0, player.col, player.row,
-                                      lvl_map.spaces[player.row][player.col].terrain, tcod.BKGND_NONE)
-                # Current position becomes next position
-                player.move(player.row_next, player.col_next)
-            else:
-                print(f'Can\'t go to {player.row_next, player.col_next}')
-            player.col_next, player.row_next = player.col, player.row
+        # Get next actor in queue
+        actor = actors.pop()
+        # Move the actors
+        actor.move(lvl_map, kp=kp)
+        # Render the actors
+        actor.render()
 
-        # Update the player position
+
+        actors.insert(0, actor)
+
+        # Update the actor position
         update_position_display(player)
         tcod.console_flush()
+
 
 def update_position_display(player):
     '''Print player position on last line of the console.'''
     tcod.console_set_default_foreground(0, tcod.green)
     tcod.console_print(
         0, 0, SCR_HEIGHT - 1, 'x=' + str(player.col).zfill(2) + ',y=' + str(SCR_HEIGHT - 1 - player.row).zfill(2))
+
 
 if __name__ == '__main__':
     main()
