65e6821c5a5156255ce3c5d24dbde283c2394a45
Scott Currie <scurrie@tableau.com>
2019-07-18 08:49:40 -0700

    Built out move and render

--- roguelike/character.py
+++ roguelike/character.py
@@ -1,3 +1,6 @@
+import abc
+import tcod
+
 class Character(object):
     def __init__(self, row, col):
         self.col = col
@@ -6,6 +9,20 @@ class Character(object):
         self.row_next = row
         self.loc = self.row, self.col
 
-    def move(self, dest_row, dest_col):
-        self.col = dest_col
-        self.row = dest_row
+    def render(self):
+        tcod.console_set_default_foreground(0, tcod.white)
+        tcod.console_put_char(0, self.col, self.row,
+                              self.symbol, tcod.BKGND_NONE)
+
+    @abc.abstractmethod
+    def move(self, lvl_map, kp=None):
+        self.row_next, self.col_next = self.get_dest(kp)
+        # Do self move if next position dfferent than current
+        if lvl_map.space_is_legal(self.row_next, self.col_next):
+            # Put terrain symbol back in vacant space
+            lvl_map.render_at(self.row, self.col)
+            # Current position becomes next position
+            self.row, self.col = self.row_next, self.col_next
+        else:
+            print(f'Can\'t go to {self.row_next, self.col_next}')
+        self.col_next, self.row_next = self.col, self.row
