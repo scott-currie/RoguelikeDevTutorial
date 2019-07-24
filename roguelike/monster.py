df7d75ce1e797f900eca7c563f44f1e625c6a0e9
Scott Currie <scurrie@tableau.com>
2019-07-24 09:26:04 -0700

    Get ref to player from actors.player now

--- roguelike/monster.py
+++ roguelike/monster.py
@@ -11,13 +11,13 @@ class Monster(Character):
 
     def get_dest(self, kp, lvl_map, actors):
         '''Find the next space to reach the player.'''
-        for actor in actors:
-            if isinstance(actor, Player):
-                player = actor
-        if self.is_adjacent_player(player):
+        # for actor in actors:
+        #     if isinstance(actor, Player):
+        #         player = actor
+        if self.is_adjacent_player(actors.player):
             return self.row, self.col
         astar = tcod.path.AStar(lvl_map)
-        _path = astar.get_path(self.col, self.row, player.col, player.row)
+        _path = astar.get_path(self.col, self.row, actors.player.col, actors.player.row)
         print(_path)
         return _path[0][1], _path[0][0]
 
