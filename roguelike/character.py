d5585dd9ad5d1c0fdd6f8402e64ca0a4ee6fb7bc
Scott Currie <scurrie@tableau.com>
2019-07-24 09:25:20 -0700

    Added update. Changed logic to determine if acted

--- roguelike/character.py
+++ roguelike/character.py
@@ -1,4 +1,5 @@
 import abc
+import logging
 import tcod
 
 
@@ -9,6 +10,16 @@ class Character(object):
         self.row = row
         self.row_next = row
         self.loc = self.row, self.col
+        self.acted = False
+
+    def update(self, kp, lvl_map, actors):
+        self.acted = False
+        logging.debug('About to move.')
+        self.move(kp, lvl_map, actors)
+        logging.debug('About to render.')
+        self.render()
+        logging.debug(f'{self} acted to False')
+        # self.acted = False
 
     def render(self):
         tcod.console_set_default_foreground(0, self.color)
@@ -17,19 +28,28 @@ class Character(object):
 
     @abc.abstractmethod
     def move(self, kp, lvl_map, actors):
-        moved = False
+        # moved = False
         self.row_next, self.col_next = self.get_dest(kp, lvl_map, actors)
-        if not hasattr(self, 'is_adjacent_player'):
-            if (self.row_next, self.col_next) == (self.row, self.col):
-                return False
+        # if not hasattr(self, 'is_adjacent_player'):
+        #     if (self.row_next, self.col_next) == (self.row, self.col):
+        #         return False
         # Do self move if next position dfferent than current
+        if hasattr(self, 'is_adjacent_player'):
+            if self.is_adjacent_player(actors.player):
+                self.acted = True
+                return
+        else:
+            if self.row_next == self.row and self.col_next == self.col:
+                return
         if lvl_map.space_is_legal(self.row_next, self.col_next):
             # Put terrain symbol back in vacant space
             lvl_map.render_at(self.row, self.col)
             # Current position becomes next position
             self.row, self.col = self.row_next, self.col_next
-            moved = True
+            # moved = True
+            logging.debug(f'{self} acted to True')
+            self.acted = True
         else:
             print(f'Can\'t go to {self.row_next, self.col_next}')
         self.col_next, self.row_next = self.col, self.row
-        return moved
+        # return moved
