01e2ca46b53f5b37e1bcf60365766b8509db9151
Scott Currie <scurrie@tableau.com>
2019-08-12 08:51:51 -0700

    Added method to return actor in a given space

--- roguelike/actors.py
+++ roguelike/actors.py
@@ -2,3 +2,9 @@ class Actors(object):
     def __init__(self):
         self.player = None
         self.queue = []
+
+    def get_actor_in_space(self, row, col):
+        for actor in self.queue:
+            if actor.row == row and actor.col == col:
+                return actor
+        return None
