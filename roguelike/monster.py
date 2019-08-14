0a26abee8c5387cc1102aa81c45a967c03ab7ea1
Scott Currie <scurrie@tableau.com>
2019-08-14 14:21:35 -0700

    changed args of get_dest.

--- roguelike/monster.py
+++ roguelike/monster.py
@@ -1,5 +1,6 @@
 from roguelike.character import Character
 from roguelike.player import Player
+import logging
 import tcod
 
 
@@ -11,6 +12,7 @@ class Monster(Character):
         self.atk = atk
         self.hp_max = hp_max
         self.hp = hp_max
+        logging.debug(f'{self} created at row={self.row}, col={self.col}')
 
     def update(self, kp, lvl_map, actors):
         self.acted = False
@@ -18,18 +20,21 @@ class Monster(Character):
         if self.is_adjacent_player(actors.player):
             self.attack(actors.player)
         else:
-            self.move(kp, lvl_map, actors)
+            self.row_next, self.col_next = self.get_dest(lvl_map, actors)
+            self.move(self.row_next, self.col_next, lvl_map, actors)
         # logging.debug('About to render.')
         self.render()
         # logging.debug(f'{self} acted to False')
         ('Monster update. Acted=', self.acted)
 
 
-    def get_dest(self, kp, lvl_map, actors):
+    def get_dest(self, lvl_map, actors):
         '''Find the next space to reach the player.'''
         if self.is_adjacent_player(actors.player):
             return self.row, self.col
         astar = tcod.path.AStar(lvl_map)
+        # logging.debug(f'{self} @ row={self.row}, col={self.col}')
+        # logging.debug(f'{actors.player} @ row={actors.player.row}, col={actors.player.col}')
         _path = astar.get_path(self.col, self.row, actors.player.col, actors.player.row)
         print(_path)
         return _path[0][1], _path[0][0]
