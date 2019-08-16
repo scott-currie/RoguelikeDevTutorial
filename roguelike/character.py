0f2d883dd0e4d7095d8f3888484917ac8a632b29
Scott Currie <scurrie@tableau.com>
2019-08-16 08:09:37 -0700

    Don't toggle walkable on move

--- roguelike/character.py
+++ roguelike/character.py
@@ -21,25 +21,22 @@ class Character(object):
                               self.symbol, tcod.BKGND_NONE)
 
     def move(self, row_dest, col_dest, lvl_map, actors):
-        # Where do you want to go?
-        # self.row_next, self.col_next = self.get_dest(kp, lvl_map, actors)
-        # Nowhere? Go back.
-        # if self.row_next == self.row and self.col_next == self.col:
-        #     return
-        # Try to move
         if lvl_map.space_is_legal(self.row_next, self.col_next):
             # Put terrain symbol back in vacant space
             lvl_map.render_at(self.row, self.col)
             # Make the space unoccupied
-            lvl_map.spaces[self.col][self.row].occupied = False
+            # lvl_map.spaces[self.row][self.col].occupied = False
+            # if lvl_map.spaces[self.row][self.col].terrain == '.':
+            #     lvl_map.walkable[self.row][self.col] = True
             # Current position becomes next position
             self.row, self.col = self.row_next, self.col_next
             # Make space occupied
-            lvl_map.spaces[self.row][self.col].occupied = True
+            # lvl_map.spaces[self.row][self.col].occupied = True
+            # Space is not walkable if there's someone on it
+            # lvl_map.walkable[self.row][self.col] = False
             # logging.debug(f'{self} acted to True')
             self.acted = True
         self.col_next, self.row_next = self.col, self.row
-
 
     def attack(self, enemy):
         print(self, 'attacked', enemy)
