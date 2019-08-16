45c93ec33f64d5c9fd3a017d3832e6ac069e0d0c
Scott Currie <scurrie@tableau.com>
2019-08-16 08:10:14 -0700

    Added make_map_walkable

--- roguelike/map.py
+++ roguelike/map.py
@@ -12,7 +12,9 @@ class Map(_Map):
         super().__init__(rows, cols, order='C')
         self.height = rows
         self.width = cols
+        self.OBSTACLES = .2
         self.spaces = self.make_spaces()
+        self.make_map_walkable()
         self.make_walls()
         self.make_obstacles()
 
@@ -25,9 +27,13 @@ class Map(_Map):
         for row in range(self.height):
             for col in range(self.width):
                 spaces[row][col] = Space(col, row)
-                self.walkable[row][col] = True
         logging.debug('Made spaces.')
         return spaces
+
+    def make_map_walkable(self):
+        for row in range(self.height):
+            for col in range(self.width):
+                self.walkable[row][col] = True
 
     def make_walls(self):
         logging.debug('Making walls.')
@@ -41,14 +47,14 @@ class Map(_Map):
     def make_obstacles(self):
         logging.debug('Making obstacles.')
         obstacles = set()
-        n_obs = int((self.height - 2) * (self.width - 2) * .2)
+        n_obs = int((self.height - 2) * (self.width - 2) * self.OBSTACLES)
         while len(obstacles) < n_obs:
             space = random.randint(
                 1, self.width - 2), random.randint(1, self.height - 2)
             obstacles.add(space)
             self.spaces[space[1]][space[0]].terrain = 'X'
             self.walkable[space[1]][space[0]] = False
-        logging.debug('Made obstaccles.')
+        logging.debug('Made obstacles.')
 
     def space_is_in_bounds(self, row, col):
         if col >= 0 and col <= self.width - 1:
@@ -57,7 +63,14 @@ class Map(_Map):
         return False
 
     def space_is_legal(self, row, col):
-        return self.space_is_in_bounds(row, col) and self.walkable[row][col]
+        # logging.debug('Looking for a legal space.')
+        if self.space_is_in_bounds(row, col):
+            if self.walkable[row][col]:
+                logging.debug('Got a walkable space inbounds.')
+                if not self.spaces[row][col].occupied:
+                    return True
+        return False
+        # return (self.space_is_in_bounds(row, col)) and (self.spaces[row][col].walkable) and (not self.spaces[row][col].occupied):
 
     def get_random_legal_space(self):
         while True:
