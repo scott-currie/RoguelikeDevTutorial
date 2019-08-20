6c092a2e25ab0acf844fe0e9e1ae528429af8667
Scott Currie <scurrie@tableau.com>
2019-08-20 16:18:28 -0700

    New monster replace code. New player creation stuff.

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ SLOW_DOWN_DELAY = 0
 
 
 def main():
+    # logging.basicConfig(level=logging.DEBUG)
     logging.basicConfig(level=logging.CRITICAL)
     tcod.console_set_custom_font(
         'arial10x10.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
@@ -31,15 +32,17 @@ def main():
     logging.debug('Ready to make player.')
     actors = Actors()
     player = Player(*lvl_map.get_random_legal_space(),
-                    PLAYER_ATK, PLAYER_MAX_HP)
+                    PLAYER_ATK, PLAYER_MAX_HP, 'Player')
     # Make space under player unwalkable
     lvl_map.walkable[player.row][player.col] = False
     actors.player = player
-    actors.queue.insert(0, player)
+    # actors.queue.insert(0, player)
+    monster_counter = 0
     for _ in range(MONSTER_COUNT):
         monster = Monster(
-            *lvl_map.get_random_legal_space(), MONSTER_ATK, MONSTER_MAX_HP)
+            *lvl_map.get_random_legal_space(), MONSTER_ATK, MONSTER_MAX_HP, f'Monster {monster_counter}')
         actors.queue.insert(0, monster)
+        monster_counter += 1
         actors.queue[0].render()
         # Make space under monster unwalkable
         lvl_map.walkable[monster.row][monster.col] = False
@@ -48,19 +51,33 @@ def main():
 
     logging.debug('Ready to enter main loop.')
     # Need to get our first actor
-    actor = actors.queue.pop()
+    actor = player
+    logging.debug(f'actor={actor}, queue={actors.queue}')
+    print(actors.queue)
     # Enter main loop
     while not tcod.console_is_window_closed():
         logging.debug(f'actor={actor}, queue={actors.queue}')
+        for _actor in actors.queue:
+            if not _actor.alive:
+                print(f'dumping {_actor}')
+                monster = Monster(
+                    *lvl_map.get_random_legal_space(), MONSTER_ATK, MONSTER_MAX_HP, f'Monster {monster_counter}')
+                monster_counter += 1
+                actors.queue.insert(0, monster)
+                actors.queue[0].render()
+                actors.queue.remove(_actor)
+                lvl_map.walkable[_actor.row][_actor.col] = True
         # Pump events for keypress
         kp = tcod.console_check_for_keypress()
         if kp.vk == tcod.KEY_ESCAPE:
             return
+        # print(player.acted)
         # Get next actor from the queue if we're ready
         if actor.acted:
             actors.queue.insert(0, actor)
             actor = actors.queue.pop()
             logging.debug(f'actor={actor}, queue={actors.queue}')
+            # print(f'{actor} appraently acted.')
             # Update the actor
         actor.update(kp, lvl_map, actors)
         # logging.debug(f'{actor} acted = {actor.acted}')
